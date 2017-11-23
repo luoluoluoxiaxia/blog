16f1f6c2003987787ee346865af7e0c803b323ce
majinlong <majinlong@huayotec.com>
2017-11-23 20:02:17 +0800

    博客页面，尚未添加评论

--- resources/views/blog/blog.blade.php
+++ resources/views/blog/blog.blade.php
@@ -3,11 +3,31 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <h1>h1. Bootstrap heading</h1>
+            <div class="col-md-8 ">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <h1>{{$blogInfo['blog_title']}}</h1>
+                        <div class="jumbotron">
+                           {!! $blogInfo['blog_content'] !!}
+                        </div>
+                    </div>
+                    <div class="panel-heading text-right ">
+                        <span style="font-size: 10px">创建时间：{{$blogInfo['created_at']}}</span>
+                    </div>
+                    <div class="panel-heading text-right">
+                        <span>分类名：{{$blogInfo['blog_type_name']}}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 ">
+                <div class="col-md-8 list-group text-center" >
+                        <a href="/list/{{$blogInfo['user_id']}}" class="list-group-item">
+                        <img src="{{$blogInfo['user_head']}}">
+                        <h4 class=" ">用户名：{{$blogInfo['user_name']}}</h4>
+                        </a>
+                </div>
+
             </div>
         </div>
     </div>
-    <script>
-    </script>
 @endsection
