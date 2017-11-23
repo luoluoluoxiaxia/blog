7eb505bc4f25f7bea1e977a622624e8895199eb9
majinlong <majinlong@huayotec.com>
2017-11-23 20:00:16 +0800

    新增了统一的错误信息的显示

--- resources/views/prompt.blade.php
+++ resources/views/prompt.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Prompt</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -13,11 +13,13 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    {{}}
-                    You are logged in!
+                    {{$mess}}
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script>
+
+</script>
 @endsection
