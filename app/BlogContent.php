d882bbb9dc3f47096b5752d688bdb7aca53628a0
majinlong <majinlong@huayotec.com>
2017-11-23 19:56:57 +0800

    新增博客的模型方法

--- app/BlogContent.php
+++ app/BlogContent.php
@@ -15,18 +15,9 @@ class BlogContent extends Model
 
     //获取用户博客列表
     public static function getBlogList($uId,$page){
-        //获取用户名称
-        $userInfo = Users::getUserInfo($uId,'user_name');
-        if(empty($userInfo)){
-            //如果查不到用户名，则认为该用户被删，直接返回空数据
-            return [];
-        }else{
-            $userName = $userInfo['user_name'];
-        }
-
         //查询数据
-        $blogList = self::where('user_id',$uId)
-            ->select('id','user_id','blog_title','created_at')
+        $blogList = self::select('id','user_id','blog_title','created_at')
+            ->where('user_id',$uId)
             ->orderBy('created_at','desc')
             ->skip($page - 1)
             ->take(self::TAKE)
@@ -35,13 +26,38 @@ class BlogContent extends Model
             return [];
         }
 
-        //组合数据
-        foreach($blogList as $key => &$val){
-            $val['user_name'] = $userName;
-        }
+        return self::_formatBlog($blogList);
 
-        return $blogList;
     }
 
 
+    //获取博客详情
+    public static function getBlog($blogId){
+        $blogInfo = self::select('id','user_id','blog_content','blog_title','blog_type_id','blog_type_name','created_at')
+            ->where('id',$blogId)
+            ->get()->toarray();
+        return current(self::_formatBlog($blogInfo));
+    }
+
+    //组合数据
+    private static function _formatBlog($blogList){
+        if(empty($blogList)){
+            return [];
+        }
+        //组合数据
+        foreach($blogList as $key => &$val){
+            if(!empty($val['user_id'])){
+                //获取用户名称
+                $userInfo = Users::getUserInfo($val['user_id'],['user_name','user_head']);
+                if(empty($userInfo)){
+                    //如果查不到用户名，则认为该用户被删，直接返回空数据
+                    unset($blogList[$key]);
+                }else{
+                    $val['user_name'] = $userInfo['user_name'];
+                    $val['user_head'] = $userInfo['user_head'];
+                }
+            }
+        }
+        return $blogList;
+    }
 }
