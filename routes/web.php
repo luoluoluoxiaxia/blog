a86103642dbbe96133da55829afb95a405057712
majinlong <majinlong@huayotec.com>
2017-11-24 18:07:55 +0800

    新增了多个路由

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::get('/list/{uid?}/{page?}','ListController@index');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/blog/{blogId?}', 'BlogController@index');
 Route::get('/push', 'BlogController@push');
+Route::post('/add_type', 'BlogTypeController@create');
+Route::post('/add_blog', 'BlogController@create');
