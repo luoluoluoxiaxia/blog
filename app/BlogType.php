0b24901141af3aacaf4d723c6987a127fcf2d31e
majinlong <majinlong@huayotec.com>
2017-11-24 18:07:32 +0800

    新增了博客分类模型

--- app/BlogType.php
+++ app/BlogType.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class BlogType extends Model
 {
     protected $table = 'blog_type';
-    //
+    //获取用户的博客类型
     public static function getUserType($uId){
         return self::select('id','type_name')
             ->where('user_id',$uId)
