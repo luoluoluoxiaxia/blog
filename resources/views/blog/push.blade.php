b177dd8a1c9d36ab6cf951d5047c65148b89951c
majinlong <majinlong@huayotec.com>
2017-11-24 18:08:28 +0800

    学会了vue的ajax，以及页面渲染。。

--- resources/views/blog/push.blade.php
+++ resources/views/blog/push.blade.php
@@ -4,39 +4,41 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 ">
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        <script id="container" name="content" type="text/plain">
-                            这里写你的初始化内容
-                        </script>
-
-                    {{--<div class="panel-heading text-right">--}}
-                        {{--<span>分类名：111</span>--}}
-                    {{--</div>--}}
-                        <div class="col-md-12" >
-                                <div class="col-md-1">
-                                    <span >分类选择</span>
-                                </div>
-                                <div class="btn-group" data-toggle="buttons" style="float: left">
-                                    @foreach( $typeList as $val)
-                                        <label class="btn btn-primary">
-                                            <input type="radio" name="options" id="option1" value="{{$val['id']}}">{{$val['type_name']}}
-                                        </label>
-                                    @endforeach
-                                </div>
-
-                            <div class="col-md-1" style="float: left">
-                                <button type="button" class="btn btn-primary"
-                                        data-toggle="button" id="addType" v-on:click="greet"> 新增
-                                </button>
+                <form action="/add_blog" id="postForm">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <div class="input-group input-group-lg col-md-12">
+                                <input type="text" class="form-control" name="blog_title" placeholder="博客标题"  v-model="blogTitle">
                             </div>
-                            <div class="input-group input-group-sm col-md-1 hidden">
-                                <input type="text" class="form-control" placeholder="新增" id="addInput">
+                            <script id="container" name="blog_content" type="text/plain" v-model="blogContent">
+                            </script>
+                            <div class="col-md-12"  >
+                                    <div class="col-md-1">
+                                        <span >分类选择</span>
+                                    </div>
+                                    <div class="btn-group" data-toggle="buttons" style="float: left">
+                                        {{--@foreach( $typeList as $val)--}}
+                                            <label class="btn btn-primary" v-for="r in blogTypeList">
+                                                <input  type="radio" name="blog_type_id" id="@{{r.id}}" value="@{{r.id}}" v-model="blogTypeId">@{{r.type_name}}
+                                            </label>
+                                        {{--@endforeach--}}
+                                    </div>
+
+                                <div class="col-md-2" style="float: left">
+                                    <button type="button" class="btn btn-primary col-md-6"
+                                            data-toggle="button"  v-on:click="greet"> 新增
+                                    </button>
+                                    <div class="input-group input-group-sm col-md-6" v-show="isShow">
+                                        <input type="text"  v-model="message"  class="form-control" placeholder="新增" id="addInput" v-on:blur="changeCount">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="input-group input-group-lg col-md-12">
+                                <button v-on:click="postForm" type="submit" class="btn btn-default text-center" style="display: block;margin-left: auto;margin-right: auto;" >baocun</button>
                             </div>
                         </div>
-                </div>
-                </div>
-
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -48,22 +50,50 @@
     </script>
     <script type="text/javascript">
         var addType = new Vue({
-            el: '#addType',
+            el: '#postForm',
             data: {
-                name: 'Vue.js'
+                name: 'Vue.js',
+                isShow:false,
+                message:null,
+                blogTypeList:@json($typeList),
+                blogTitle:'',
+                blogContent:'',
+                blogTypeId:'',
             },
-            // 在 `methods` 对象中定义方法
             methods: {
                 greet: function (event) {
-                    // `this` 在方法里指向当前 Vue 实例
-                    alert('Hello ' + this.name + '!')
-                    // `event` 是原生 DOM 事件
-                    if (event) {
-                        alert(event.target.tagName)
+                    this.isShow = true
+                },
+                changeCount: function (value){
+                    if( this.message == '' || this.message == null){
+                        //可以获取ue编辑器的内容 alert(ue.getContent());
+                        alert("请输入分类");
+                        return;
                     }
+                    //得到新的分类，将他通过ajax插入到数据库中
+                    let data = [
+                        ["type", this.message],
+                    ];
+                    let url_params = new URLSearchParams(data);
+                    axios.post('/add_type', url_params).then((response) => {
+                        if(response.data.status == 1){
+                            this.blogTypeList.push({"id":response.data.id,"type_name":this.message})
+
+                        }
+                    }).catch(function (error) {
+                    });
                 }
             }
         })
+//        var postForm = new Vue({
+//            el: '#',
+//            data:{
+//
+//            },
+//            postForm : function (){
+//                alert(this.blog_type_id);
+//            }
+//        })
     </script>
 
 @endsection
