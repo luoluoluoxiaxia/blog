6be45ceccb477ab95173db64057e2e610caceb98
majinlong <majinlong@huayotec.com>
2017-11-24 18:09:04 +0800

    无用的一个提交

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -50,7 +50,7 @@ class BlogController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
         //
     }
